8b165ce6baec6bb316ff3ea57f23eabdcd4cdb18
Roman Zahorskyi <61247961+disertus@users.noreply.github.com>
2021-11-18 00:18:54 +0200

    Add inserts for postgres

--- speedtest_telemetry.go
+++ speedtest_telemetry.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
 	"os"
 	"os/exec"
 	"time"
-	"log"
 )
 
 type Speedtest struct {
@@ -48,32 +50,84 @@ type Speedtest struct {
 		Id  string
 		Url string
 	} `json:"result"`
-} 
+}
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = USER
+	password = PASS
+	dbname   = DBNAME
+)
 
+func insertIntoDB(db *sql.DB, data Speedtest) {
+	sqlStatement := `
+			INSERT INTO speedtests (id, created_at, type, jitter, latency, download_bandwidth, download_bytes, download_elapsed,
+				upload_bandwidth, upload_bytes, upload_elapsed, packet_loss, isp, internal_id, interface_name, mac_address, is_vpn,
+				external_ip, server_id, server_name, server_location, server_country, server_host, server_port, server_ip, speedtest_url)
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26)`
 
-func main() {
-	path, err := os.Getwd()
+	_, err := db.Exec(sqlStatement, data.Result.Id, data.Timestamp, data.Type, data.Ping.Jitter, data.Ping.Latency,
+		data.Download.Bandwidth, data.Download.Bytes, data.Download.Elapsed, data.Upload.Bandwidth, data.Upload.Bytes,
+		data.Upload.Elapsed, data.PacketLoss, data.Isp, data.Interface.InternalIp, data.Interface.Name, data.Interface.MacAddr,
+		data.Interface.IsVpn, data.Interface.ExternalIp, data.Server.Id, data.Server.Name, data.Server.Location, data.Server.Country,
+		data.Server.Host, data.Server.Port, data.Server.Ip, data.Result.Url)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
+	return
+}
+
+func runSpeedtest(path string) []byte {
 	output, err := exec.Command(path+"/speedtest", "-f", "json-pretty").Output()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	return output
+}
 
-	fmt.Println(string(output))
+func scheduleSpeedtest() {
+	panic("Not implemented")
+}
 
-	var data Speedtest
-	err = json.Unmarshal(output, &data)
-	if err != nil {
-		log.Fatalf("Error occurred while unmarshalling: %s", err.Error())
+func main() {
+	for {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		var output []byte
+		output = runSpeedtest(path)
+
+		fmt.Println(string(output))
+
+		var data Speedtest
+		err = json.Unmarshal(output, &data)
+		if err != nil {
+			log.Fatalf("Error occurred while unmarshalling: %s", err.Error())
+		}
+
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbname)
+		db, err := sql.Open("postgres", psqlInfo)
+		if err != nil {
+			panic(err)
+		}
+
+		defer db.Close()
+
+		insertIntoDB(db, data)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Successfully connected!")
+		fmt.Println("Sleeping for 20 secs")
+		time.Sleep(1800 * time.Second)
 	}
 
-	// fmt.Println(data.Download.Bandwidth * 8 / 1000000)
-	// fmt.Println(data.Upload.Bandwidth * 8 / 1000000)
-	// fmt.Println(data.Result.Url)
-
-	// time.Sleep(20 * time.Second)
 }
